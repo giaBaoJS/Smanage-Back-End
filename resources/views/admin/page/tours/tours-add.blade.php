bfde2b3c734548af79bede739e83c9b32a80e420
paul <waycaosi1994@gmail.com>
2020-11-23 22:10:20 +0700

    tour

--- resources/views/admin/page/tours/tours-add.blade.php
+++ resources/views/admin/page/tours/tours-add.blade.php
@@ -11,151 +11,136 @@
 </div>
 @endsection
 @section('content')
-<div class="page-content-wrapper">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xl-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="header-title">Thêm tours</h4>
-                        <p class="card-title-desc">
-                            Tạo coupon tạo ưu đãi cho người dùng
-                        </p>
-
-                        <form class="custom-validation" action="#">
-                            <div class="form-group">
-                                <label>Tên tours</label>
-                                <input
-                                    type="text"
-                                    class="form-control"
-                                    placeholder="Trải nghiệm Mù Cang Chải 5 ngày 4 đêm"
-                                    required
-                                />
-                            </div>
-
-                            <!-- chọn miền gọi ajax ra tỉnh -->
-                            <div class="form-group">
-                                <label>Miền</label>
-                                <select class="custom-select" required>
-                                    <option value="">Chọn miền</option>
-                                    <option value="1">Miền Bắc</option>
-                                    <option value="2">Miền Trung</option>
-                                    <option value="3">Miền Nam</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label>Tỉnh</label>
-                                <select class="custom-select" required>
-                                    <option value="">Chọn tỉnh</option>
-                                    <option value="1">Sài Gòn</option>
-                                    <option value="2">Wakanda</option>
-                                    <option value="3">Cần Thơ</option>
-                                </select>
-                            </div>
-                            <!-- chọn hình thức di chuyển gọi ajax ra nhà vận chuyển -->
-                            <div class="form-group">
-                                <label>Hình thức di chuyển</label>
-                                <select class="custom-select" required>
-                                    <option value="">
-                                        Lựa chọn hình thức di chuyển
-                                    </option>
-                                    <option value="1">Máy bay</option>
-                                    <option value="2">Xe 50 chỗ</option>
-                                    <option value="3">Xe giường nằm</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label>Nhà vận chuyển</label>
-                                <select class="custom-select" required>
-                                    <option value="">Chọn nhà vận chuyển</option>
-                                    <option value="1">Vietnam Airline</option>
-                                    <option value="2">Thành Bưởi</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label>Ngày và giờ</label>
-                                <div>
-                                    <input
-                                        class="form-control datepicker-here"
-                                        placeholder="01/10/2020 - 10/10/2020"
-                                        id="coupon-time"
-                                        data-range="true"
-                                        data-multiple-dates-separator=" - "
-                                        data-language="vi"
-                                        required
-                                    />
-                                </div>
-                            </div>
-                            <!-- Giá trẻ em sẽ bằng số % giá người lớn -->
-                            <div class="form-group">
-                                <label>Giá</label>
-                                <input
-                                    type="text"
-                                    class="form-control"
-                                    placeholder="40.000.000 VNĐ"
-                                    required
-                                />
-                            </div>
-                            <div class="form-group">
-                                <label>Ảnh đại diện tours</label>
-
-                                <div class="custom-file">
-                                    <input
-                                        type="file"
-                                        class="custom-file-input"
-                                        required
-                                    />
-                                    <label class="custom-file-label"
-                                        >Chọn ảnh đại diện...</label
-                                    >
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label>Thêm ảnh vào thư viện</label>
-
-                                <div class="custom-file">
-                                    <input
-                                        type="file"
-                                        multiple
-                                        class="custom-file-input"
-                                        required
-                                    />
-                                    <label class="custom-file-label"
-                                        >Chọn nhiều ảnh...</label
-                                    >
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label>Giới thiệu tours</label>
-                                <textarea
-                                    id="elm1"
-                                    name="area"
-                                    placeholder="Nhập mô tả"
-                                ></textarea>
-                            </div>
-                            <div class="form-group mb-0">
-                                <div>
-                                    <button
-                                        type="submit"
-                                        class="btn waves-effect waves-light mr-1 white-cl bg-main-cl"
-                                    >
-                                        Thêm
-                                    </button>
-                                    <button
-                                        type="reset"
-                                        class="btn btn-secondary waves-effect"
-                                    >
-                                        Hủy
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+<div class="add-form-tours">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="items  tabl1 active" data-tab="tab1">
+                <span>1. Thêm Tour</span>
             </div>
         </div>
-        <!-- end row -->
+        <div class="col-md-4">
+            <div class="items tabl2" data-tab="tab2">
+                <span>2. Thêm Lịch trình</span>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="items tabl3" data-tab="tab3">
+                <span>3. Hoàn thành</span>
+            </div>
+        </div>
     </div>
-    <!-- end container-fluid -->
+    <form action="#">
+    <div class="group-forms  tabsz tab1 active">
+        <h3 class="title-cm">Thông tin tours</h3>
+            <div class="form-group">
+                <label for="name">Tên Tours</label>
+                <input type="text" class="form-control"  placeholder="Nhập tên tour">
+            </div>
+            <div class="form-group">
+                <label for="name">Mô tả</label>
+                <input type="text" class="form-control"  placeholder="Nhập mô tả">
+            </div>
+            <div class="form-group">
+                <label for="name">Giờ đi</label>
+                <input type="text" class="form-control"  placeholder="Giờ đi">
+            </div>
+            <div class="form-group">
+                <label for="name">Ngày đi</label>
+                <input type="date" class="form-control"  placeholder="Nhập Ngày đi">
+            </div>
+            <div class="form-group">
+                <label for="name">Ngày về</label>
+                <input type="date" class="form-control"  placeholder="Nhập Ngày về">
+            </div>
+            <div class="form-group">
+                <label for="name">Phương tiện</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                    <option value="1">Máy bay</option>
+                    <option value="2">Xe Khách</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="name">Giá mặc định</label>
+                <input type="text" class="form-control"  placeholder="Nhập giá">
+            </div>
+            <div class="form-group">
+                <label for="name">Giá trẻ em</label>
+                <input type="text" class="form-control"  placeholder="Nhập giá">
+            </div>
+            <div class="form-group">
+                <label for="name">Giá trẻ em</label>
+                <input type="text" class="form-control"  placeholder="Nhập giá">
+            </div>
+            <div class="form-group">
+                <label for="name">Giảm giá coupon </label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                    <option value="1">SALE20</option>
+                    <option value="2">SALE40</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="name">Số lượng người</label>
+                <input type="number" class="form-control"  placeholder="Nhập tối đa số người tham gia">
+            </div>
+            <div class="form-group">
+                <label>Ảnh đại diện cho tour</label>
+                    <div class="custom-file">
+                         <input
+                          type="file"
+                           class="custom-file-input"
+                           required
+                           />
+                         <label class="custom-file-label"
+                        >Chọn ảnh đại diện...</label >
+                    </div>
+            </div>
+        <div class="group-my-btn">
+        <a class="mybtn"  href="/admin/dashboard">Hủy</a>
+        <a class="mybtn tab1"  data-tab="tab2">Tiếp theo</a>
+        </div>
+    </div>
+    <div class="group-forms  tabsz tab2">
+        <h3 class="title-cm">Thông tin lịch trình</h3>
+            <div class="form-group">
+                <label for="name">Số Ngày</label>
+                <input type="text" class="form-control"  placeholder="Nhập số ngày">
+            </div>
+            <div class="form-group">
+                <label>Giới thiệu tours</label>
+                 <textarea
+                    id="elm1"
+                    name="area"
+                    placeholder="Nhập mô tả"
+                    ></textarea>
+            </div>
+            <div class="form-group">
+                <label>Ảnh cho lịch trình</label>
+                    <div class="custom-file">
+                         <input
+                          type="file"
+                           class="custom-file-input"
+                           required
+                           />
+                         <label class="custom-file-label"
+                        >Chọn ảnh lịch trình...</label >
+                    </div>
+            </div>
+            <div class="group-my-btn">
+            <a class="mybtn tab2-ql"  data-tab="tab1">Quay lại</a>
+                <a class="mybtn tab2"  data-tab="tab3">Tiếp theo</a>
+            </div>
+    </div>
+    <div class="group-forms  tabsz tab3">
+        <h3 class="title-cm">Xác nhận thông tin đầy đủ</h3>
+        <p>
+           Vui lòng kiểm tra lại đầy đủ thông tin trước khi gửi. Xin cảm ơn !
+        </p>
+        <div class="group-my-btn">
+            <a class="mybtn tab3"  data-tab="tab2">Quay lại</a>
+            <button class="mybtn" type="submit">Hoàn Thành</button>
+        </div>
+    </div>
+    </div>
+    </form>
 </div>
 @endsection
